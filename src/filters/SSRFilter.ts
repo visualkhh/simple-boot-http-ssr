1a62977544d2a70f23b61975bb6f9cd568e5ee20
visualkhh <visualkhh@gmail.com>
2021-12-18 17:03:18 +0900

    patch http

--- src/filters/SSRFilter.ts
+++ src/filters/SSRFilter.ts
@@ -6,24 +6,22 @@ import {RequestResponse} from '../models/RequestResponse';
 import {SimpleBootFront} from 'simple-boot-front/SimpleBootFront';
 import {HttpStatus} from '../codes/HttpStatus';
 import {HttpHeaders} from '../codes/HttpHeaders';
+import { OnDoneRoute } from 'simple-boot-front/route/OnDoneRoute';
 
-export class SSRFilter implements Filter {
+export class SSRFilter implements Filter, OnDoneRoute {
     private navigation: Navigation;
 
     constructor(private simpleBootFront: SimpleBootFront) {
         this.navigation = simpleBootFront.getSimstanceManager().getOrNewSim(Navigation)!
+        this.simpleBootFront.regDoneRouteCallBack(this);
     }
 
     async before(req: IncomingMessage, res: ServerResponse) {
         const rr = new RequestResponse(req, res)
-        const url = rr.reqUrl ?? '';
         // if ((rr.req.headers.accept ?? Mimes.TextHtml).indexOf(Mimes.TextHtml) >= 0) {
         if (rr.reqHasAcceptHeader(Mimes.TextHtml)) {
-            this.navigation.go(url);
-            const header = {} as any;
-            header[HttpHeaders.ContentType] = Mimes.TextHtml;
-            rr.res.writeHead(HttpStatus.Ok, header);
-            rr.res.end(document.documentElement.outerHTML);
+            this.simpleBootFront.pushDoneRouteCallBack(this, rr);
+            this.navigation.go(rr.reqUrl);
             return false;
         } else {
             return true;
@@ -32,6 +30,13 @@ export class SSRFilter implements Filter {
 
     async after(req: IncomingMessage, res: ServerResponse) {
         return true;
+    }
+
+    onDoneRoute(rr: RequestResponse): void {
+        const header = {} as any;
+        header[HttpHeaders.ContentType] = Mimes.TextHtml;
+        rr.res.writeHead(HttpStatus.Ok, header);
+        rr.res.end(document.documentElement.outerHTML);
     }
 
 }
