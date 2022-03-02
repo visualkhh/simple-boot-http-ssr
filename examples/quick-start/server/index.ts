efaa353f58906b89b0c78117425db32406e88126
visualkhh <visualkhh@gmail.com>
2022-03-03 05:19:03 +0900

    example and readme

--- examples/quick-start/server/index.ts
+++ examples/quick-start/server/index.ts
@@ -1,26 +1,41 @@
 import 'reflect-metadata';
-import {IndexRouter} from '@server/app/index.router';
-import {SimpleBootHttpSSRServer} from 'simple-boot-http-ssr/SimpleBootHttpSSRServer';
 import {SSRFilter} from 'simple-boot-http-ssr/filters/SSRFilter';
-import {MakeSimFrontOption} from '@src/bootfactory';
+import Factory, {MakeSimFrontOption} from '@src/bootfactory';
 import {ConstructorType} from 'simple-boot-core/types/Types';
-import {RequestResponse} from 'simple-boot-http-server/models/RequestResponse';
-const using: ConstructorType<any>[] = [
+import {HttpServerOption} from 'simple-boot-http-server/option/HttpServerOption';
+import {SimpleBootHttpSSRServer} from 'simple-boot-http-ssr';
+import {AppRouter} from '@src/app.router';
+import {ResourceFilter} from 'simple-boot-http-server/filters/ResourceFilter';
+
+const using: ConstructorType<any>[] = [];
+
+const otherInstanceSim = new Map<ConstructorType<any>, any>();
+const option = new HttpServerOption();
+const frontDistPath = 'dist-front';
+option.filters = [
+    new ResourceFilter(frontDistPath,
+        ['\.js$', '\.map$', '\.ico$', '\.png$', '\.jpg$', '\.jpeg$', '\.gif$', 'offline\.html$', 'webmanifest$', 'manifest\.json', 'service-worker\.js$', 'googlebe4b1abe81ab7cf3\.html$']
+    ),
+    new SSRFilter({
+        frontDistPath: frontDistPath,
+        factorySimFrontOption: (window: any) => MakeSimFrontOption(window),
+        factory: Factory,
+        poolOption: {
+            max: 10,
+            min: 1
+        },
+        using,
+        domExcludes: []
+    }, otherInstanceSim)
 ];
-new SSRFilter({
-    frontDistPath: 'dist-front',
-    factorySimFrontOption: (window: any) => MakeSimFrontOption(window),
-    factory: Factory,
-    poolOption: {
-        max: 50,
-        min: 1
-    },
-    using,
-    domExcludes: []
-}, otherInstanceSim)
+option.listen.listeningListener = (server: SimpleBootHttpSSRServer) => {
+    console.log(`startup server ${server.option.listen.port}`);
+}
 
-const ssr = new SimpleBootHttpSSRServer(IndexRouter, option);
-await ssr.run(otherInstanceSim);
-return ssr;
+new SimpleBootHttpSSRServer(AppRouter, option).run(otherInstanceSim);
 
-console.log('server startUp finish!!', it.option.listen);
+// const ssr = new SimpleBootHttpSSRServer(IndexRouter, option);
+// await ssr.run(otherInstanceSim);
+// return ssr;
+//
+// console.log('server startUp finish!!', it.option.listen);
--- examples/quick-start/src/app.router.ts
+++ examples/quick-start/src/app.router.ts
@@ -2,13 +2,28 @@ import {Sim} from 'simple-boot-core/decorators/SimDecorator';
 import {Router} from 'simple-boot-core/decorators/route/Router';
 import {Component} from 'simple-boot-front/decorators/Component';
 import template from './app.router.html';
+import {MainPage} from '@src/pages/main.page';
+import {SubPage} from '@src/pages/sub.page';
+import {RouterAction} from 'simple-boot-core/route/RouterAction';
+import {Intent} from 'simple-boot-core/intent/Intent';
 @Sim()
 @Router({
-    path: ''
+    path: '',
+    route: {
+        '/': MainPage,
+        '/sub': SubPage,
+    }
 })
 @Component({
     template: template
 })
-export class AppRouter {
+export class AppRouter implements RouterAction {
+    private child: any;
+    async canActivate(url: Intent, module: any) {
+        this.child = module;
+    }
 
+    hasActivate(checkObj: any): boolean {
+        return this.child === checkObj;
+    }
 }
