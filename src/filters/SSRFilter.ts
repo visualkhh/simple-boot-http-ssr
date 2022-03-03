e9fe9f93aec84bb1428833920a300196070aa5dd
khh <khh@omnicns.com>
2022-03-03 15:36:52 +0900

    ssrfilter

--- src/filters/SSRFilter.ts
+++ src/filters/SSRFilter.ts
@@ -10,6 +10,8 @@ import {SimpleBootHttpServer} from 'simple-boot-http-server';
 import {SimFrontOption} from 'simple-boot-front/option/SimFrontOption';
 import {SimpleBootFront} from 'simple-boot-front/SimpleBootFront';
 import {AsyncBlockingQueue} from 'simple-boot-core/queues/AsyncBlockingQueue';
+import {RandomUtils} from 'simple-boot-core/utils/random/RandomUtils';
+import * as JSDOM from 'jsdom';
 
 export type FactoryAndParams = {
     frontDistPath: string;
@@ -25,7 +27,7 @@ export type FactoryAndParams = {
     ssrExcludeFilter?: (rr: RequestResponse) => boolean;
 }
 export class SSRFilter implements Filter {
-    private simpleBootFrontPool: SimpleBootFront[] = [];
+    private simpleBootFrontPool: Map<string, SimpleBootFront> = new Map();
     private simpleBootFrontQueue = new AsyncBlockingQueue<SimpleBootFront>();
     private indexHTML: string;
     private welcomUrl = 'http://localhost'
@@ -38,30 +40,48 @@ export class SSRFilter implements Filter {
         for (let i = 0; i < this.factory.poolOption.min; i++) {
             await this.pushQueue()
         }
+        console.log('SimpleBootHttpSSRFactory init success ', + this.simpleBootFrontPool.size)
     }
 
-    async pushQueue() {
-        if (this.simpleBootFrontPool.length < this.factory.poolOption.max) {
-            const idx = this.simpleBootFrontPool.length
-            const jsdom = await new JsdomInitializer(this.factory.frontDistPath, this.factory.frontDistIndexFileName || 'index.html', {url: this.welcomUrl}).run();
-            const window = jsdom.window as unknown as Window & typeof globalThis;
-            (window as any).ssrUse = false;
-            const option = this.factory.factorySimFrontOption(window);
-            option.name = `SSRFilter-pool-${idx + 1}`
-            const simpleBootFront = await this.factory.factory.create(option, this.factory.using, this.factory.domExcludes);
-            simpleBootFront.run(this.otherInstanceSim);
-            this.simpleBootFrontPool.push(simpleBootFront);
-            this.simpleBootFrontQueue.enqueue(simpleBootFront);
+    async makeJsdom() {
+        const jsdom = await new JsdomInitializer(this.factory.frontDistPath, this.factory.frontDistIndexFileName || 'index.html', {url: this.welcomUrl}).run();
+        return jsdom;
+    }
+
+    async makeFront() {
+        const name = RandomUtils.uuid();
+        const jsdom = await this.makeJsdom();
+        const window = jsdom.window as unknown as Window & typeof globalThis;
+        (window as any).ssrUse = false;
+        const option = this.factory.factorySimFrontOption(window);
+        option.name = name;
+        const simpleBootFront = await this.factory.factory.create(option, this.factory.using, this.factory.domExcludes);
+        simpleBootFront.run(this.otherInstanceSim);
+        (simpleBootFront as any).jsdom = jsdom;
+        return simpleBootFront;
+    }
+
+    enqueueFrontApp(simpleBootFront: SimpleBootFront) {
+        this.simpleBootFrontPool.set(simpleBootFront.option.name!, simpleBootFront);
+        this.simpleBootFrontQueue.enqueue(simpleBootFront);
+    }
+
+    async pushQueue(destorFront?: SimpleBootFront) {
+        if (destorFront) {
+            this.simpleBootFrontPool.delete(destorFront.option.name!);
+        }
+        if (this.simpleBootFrontPool.size < this.factory.poolOption.max) {
+            this.enqueueFrontApp(await this.makeFront());
         }
     }
 
     async before(rr: RequestResponse) {
-        // const now = Date.now();
-        // console.log('SSRFilter before start===================', this.simpleBootFrontQueue.isEmpty(), this.simpleBootFrontPool.length, now);
+        const now = Date.now();
+        // console.log('SSRFilter before start===================',rr.reqUrl, this.simpleBootFrontQueue.isEmpty(), this.simpleBootFrontPool.size, now);
         // 무조건 promise로 await 해야 함
         if ((rr.reqHasAcceptHeader(Mimes.TextHtml) || rr.reqHasAcceptHeader(Mimes.All))) {
             if (this.factory.ssrExcludeFilter?.(rr)) {
-                this.writeOkHtmlAndEnd({rr}, this.indexHTML);
+                await this.writeOkHtmlAndEnd({rr}, this.indexHTML);
                 return false;
             }
             if (this.simpleBootFrontQueue.isEmpty()) {
@@ -95,11 +115,33 @@ export class SSRFilter implements Filter {
             } finally {
                 (simpleBootFront.option.window as any).ssrUse = false;
                 delete (simpleBootFront.option.window as any).server_side_data;
-                simpleBootFront.option.window.location.href = this.welcomUrl;
-                this.simpleBootFrontQueue.enqueue(simpleBootFront);
+                // ((simpleBootFront as any).jsdom as JSDOM.JSDOM)?.reconfigure({url: this.welcomUrl });
+                // simpleBootFront.option.window.document.body.innerHTML = this.rootJSDOM.window.document.body.innerHTML;
+                // simpleBootFront.writeRootRouter()
+                // await simpleBootFront.goRouting('/');
+
+                // ((simpleBootFront as any).jsdom as JSDOM.JSDOM)?.reconfigure({url: this.welcomUrl });
+
+                // simpleBootFront.option.window.location.href = this.welcomUrl;
+                // simpleBootFront.option.window.document.documentElement.outerHTML = this.indexHTML;
+                // this.simpleBootFrontQueue.enqueue(simpleBootFront);
+                this.pushQueue(simpleBootFront).then(it => {
+                    // console.log('deee')
+                });
+                // console.log('-1->', simpleBootFront.option.window.location.href);
+                // simpleBootFront.option.window.location.href = this.welcomUrl;
+                // simpleBootFront.option.window.location.reload();
+                // console.log('-2->', simpleBootFront.option.window.location.href);
+                // simpleBootFront.option.window.location.reload();
+                // console.log('-3->', simpleBootFront.option.window.location.href);
+                // this.simpleBootFrontQueue.enqueue(simpleBootFront);
+                // await this.makeJsdom();
+                // simpleBootFront.option.window.location.href = this.welcomUrl;
             }
+            // console.log('----doen')
             return false;
         } else {
+            // console.log('----doen2')
             return true;
         }
     }
@@ -112,7 +154,7 @@ export class SSRFilter implements Filter {
     }
 
     async after(rr: RequestResponse, app: SimpleBootHttpServer, sw: boolean) {
-        // console.log('----------', sw)
+        // console.log('done--------', sw)
         return sw;
     }
 
