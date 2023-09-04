b883d67056693cc22fac894679b30323eb0b30a0
visual <visual@jaranda.kr>
2023-09-04 20:21:18 +0900

    bugfix

--- src/SimpleBootHttpSSRServer.ts
+++ src/SimpleBootHttpSSRServer.ts
@@ -1,6 +1,7 @@
 import {ConstructorType} from 'simple-boot-core/types/Types';
 import { SimpleBootHttpServer } from 'simple-boot-http-server/SimpleBootHttpServer';
 import { HttpServerOption } from 'simple-boot-http-server/option/HttpServerOption';
+import { SimstanceManager } from 'simple-boot-core/simstance/SimstanceManager';
 
 export class SimpleBootHttpSSRServer extends SimpleBootHttpServer {
     constructor(rootRouter: ConstructorType<Object>, option?: HttpServerOption) {
@@ -8,7 +9,7 @@ export class SimpleBootHttpSSRServer extends SimpleBootHttpServer {
     }
 
     // @ts-ignore
-    run(otherInstanceSim?: Map<ConstructorType<any>, any>) {
+    run(otherInstanceSim?: Map<ConstructorType<any>, any>): SimstanceManager {
         const oi = new Map<ConstructorType<any>, any>()
         otherInstanceSim?.forEach((value, key) => oi.set(key, value));
         return super.run(oi);
